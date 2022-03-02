97fde50bf7613f02f16fccc0de1de617772b6936
Tim Trimble <tim.taznar@gmail.com>
2022-03-02 10:37:50 -0700

    Added RelationshipAgent class

---
+++ TraceryPlugin/TraceryPlugin/RelationshipAgent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraceryPlugin
+{
+    class RelationshipAgent
+    {
+        private static int next_id_ = 0;
+
+        private int my_id_;
+
+        public RelationshipAgent()
+        {
+            my_id_ = next_id_++;
+        }
+    }
+}
