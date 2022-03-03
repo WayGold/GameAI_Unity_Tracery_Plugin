498d700b49762c547ca975d07f3afbe15a029d36
Randy Chang <u1302280@ad.utah.edu>
2022-03-03 02:07:27 -0700

    Added simple dialogue test scene

---
+++ TraceryPluginUnity/Assets/Scripts/Character.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Character : MonoBehaviour
+{
+    public TMPro.TextMeshProUGUI sentenceGUI;
+    public bool speaking;
+}
---
+++ TraceryPluginUnity/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Character char1;
+    public Character char2;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+ 
+    }
+
+    // This is the function that somehow processes a sentence thru Tracery
+    // (takes in consider the character's relationship)
+    string ProcessInputSentence(string sentenceFromOtherCharater)
+    {
+        // example of processing sentence
+        return sentenceFromOtherCharater + " " + Random.Range(0, 20);
+    }
+
+    // On continue button click event
+    // we should decide who speaks now
+    // and process the input sentence thru tracery
+    public void OnConversationContinueButtonClick()
+    {
+        string input;
+
+        // find out who was previously speaking, get that sentence as input to tracery
+        if (char1.speaking)
+        {
+            input = char1.sentenceGUI.text;
+            string output = ProcessInputSentence(input);
+            char2.sentenceGUI.text = output;
+            
+        }
+        else if (char2.speaking)
+        {
+            input = char2.sentenceGUI.text;
+            string output = ProcessInputSentence(input);
+            char1.sentenceGUI.text = output;
+        }
+
+        ToggleSpeakingCharacter();
+
+        
+
+    }
+
+    void ToggleSpeakingCharacter()
+    {
+        char1.speaking = !char1.speaking;
+        char2.speaking = !char2.speaking;
+    }
+
+    
+
+}
